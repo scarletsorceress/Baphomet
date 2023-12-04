17ee2bfc068dae3fa2dcb0a7513d9ecb92c94e01
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2023-12-04 17:49:45 -0300

    paginas novas ajustes de scrollbar e organizaçao

---
+++ javinha/home.js
@@ -0,0 +1,48 @@
+const animationContainer = document.querySelector('.animation-container');
+const mainSection = document.getElementById('main-section');
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    //checa se a animação ja foi executada uma vez para o usuario
+
+    if(localStorage.getItem('animationPlayed') === null) {
+
+        // evento que aciona a animaçao quando a pagina carrega
+        animationContainer.classList.add('show');
+        
+        // definindo funçao pra rolar a pagina de forma lenta e suave
+        function scrollTo(element, to, duration) {
+            if (duration <= 0) return;
+            const difference = to - window.scrollY;
+            const perTick = difference / duration * 10;
+
+            setTimeout(function () {
+                window.scroll(0, window.scrollY + perTick);
+                if (window.scrollY === to) return;
+                scrollTo(element, to, duration - 10);
+            }, 1);
+        }
+        
+        // da um fade no container depois de um certo tempo
+        setTimeout(() => {
+            animationContainer.classList.remove('show');
+            
+            // rola pra baixo da tela em um elemento especifico, mas rola lentamente
+            scrollTo(mainSection, window.innerHeight, 2000); // Adjust the duration (e.g., 3000 milliseconds) for slower scrolling
+            setTimeout(() => {animationContainer.style.maxHeight = 0}, 3000);
+        }, 3000);
+
+        /*
+        seta um item no localStorage depois que a animação foi executada um vez, 
+        desta forma, quando o usuario voltar a pagina,
+        não vai precisar ver a animação novamente
+        */
+        localStorage.setItem('animationPlayed', JSON.stringify(true));
+
+    } else {
+        // se a animação ja foi executada uma vez, esconde o container da animação
+        animationContainer.style.display = 'none';
+    }
+});
+
+
---
+++ javinha/menuButton.js
@@ -0,0 +1,42 @@
+// Get references to the button and menu
+const menuButton = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+const isMenuOpen = false;
+
+menuButton.addEventListener('click', () => { menuButtonChange(); menuButtonTransition(); }); 
+
+function menuButtonTransition() {
+    if (menu.style.maxHeight === '0px' || menu.style.maxHeight === '') {
+        // abre o menu
+        menu.style.display = 'block';
+        menu.style.maxHeight = menu.scrollHeight + 'px';
+    } else {
+        // fecha o menu
+        menu.style.display = 'none';
+        menu.style.maxHeight = '0';
+    }
+};
+
+
+function menuButtonChange() {
+    menuButton.classList.toggle('change');
+};
+
+// fecha o menu quando um item do menu é clicado
+const menuItems = menu.querySelectorAll('li');
+menuItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+        menu.style.display = 'none';
+    });
+});
+
+
+// fecha o menu quando clica em qualquer lugar da tela
+window.onclick = function(event) {
+    if (event.target !== menuButton) {
+        menu.style.display = 'none';
+        menu.style.maxHeight = '0';
+        menuButton.classList.remove('change');
+    }
+}
